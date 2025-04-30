09f41dce4ad550358a693f3766e3633002bf2a15
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-04-30 23:01:28 +0300

    Refactor SplashScreen layout for improved readability and maintainability

--- generate_image/lib/screens/splash_screen.dart
+++ generate_image/lib/screens/splash_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    //wait for some time on splash & then move to next screen
+    // wait for some time on splash & then move to next screen
     Future.delayed(const Duration(seconds: 2), () {
       Get.off(() =>
           Pref.showOnboarding ? const OnboardingScreen() : const HomeScreen());
@@ -25,41 +25,12 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    //initializing device size
+    // initializing device size
     mq = MediaQuery.sizeOf(context);
 
     return Scaffold(
-      //body
-      body: SizedBox(
-        width: double.maxFinite,
-        child: Column(
-          children: [
-            //for adding some space
-            const Spacer(flex: 2),
-
-            //logo
-            Card(
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: Padding(
-                padding: EdgeInsets.all(mq.width * .05),
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: mq.width * .4,
-                ),
-              ),
-            ),
-
-            //for adding some space
-            const Spacer(),
-
-            //lottie loading
-            const CustomLoading(),
-
-            //for adding some space
-            const Spacer(),
-          ],
-        ),
+      body: Center(
+        child: CustomLoading(),
       ),
     );
   }
