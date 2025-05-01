6ecd8baf84bd2c6e06576321e9cdc61e1fd2c5de
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-05-01 13:02:20 +0300

    Initialize Hive and add Pref helper class for managing app preferences

--- generate_image/lib/main.dart
+++ generate_image/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generate_image/helper/pref.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // init hive
+  await Pref.initialize();
+
+  // for app write initialization
+  AppWrite.init();
+
+  // for initializing facebook ads sdk
+  AdHelper.init();
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
