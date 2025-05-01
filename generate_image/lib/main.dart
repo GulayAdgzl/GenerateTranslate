e9b4d4fcdb9ea14d6c07e00befbe6dcb2caadc27
Gülay ADIGÜZEL <glyadgzl@hotmail.com>
2025-05-01 13:06:44 +0300

    Add AdHelper and MyDialog classes for ad management and loading dialogs

--- generate_image/lib/main.dart
+++ generate_image/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generate_image/helper/ad_helper.dart';
 import 'package:generate_image/helper/pref.dart';
 
 Future<void> main() async {
